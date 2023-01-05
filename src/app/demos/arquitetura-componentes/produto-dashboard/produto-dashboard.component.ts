fc76850621f7b01b08bdf039d500eb91831bde8d
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2023-01-05 20:39:26 -0300

    feat: ViewChildren e ViewChild

--- src/app/demos/arquitetura-componentes/produto-dashboard/produto-dashboard.component.ts
+++ src/app/demos/arquitetura-componentes/produto-dashboard/produto-dashboard.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { fromEvent, Observable } from 'rxjs';
+import { Component, ElementRef, ViewChild, AfterViewInit, OnInit, ViewChildren, QueryList } from '@angular/core';
 import { Produto } from '../models/produto';
+import { ProdutoCountComponent } from '../componentes/produto-count.component';
+import { ProdutoCardDetalheComponent } from '../componentes/produto-card-detalhe.component';
 
 @Component({
   selector: 'app-produto-dashboard',
   templateUrl: './produto-dashboard.component.html'
 })
-export class ProdutoDashboardComponent {
+export class ProdutoDashboardComponent implements OnInit, AfterViewInit {
 
   produtos!: Produto[]
+
+  //NAO EH RECOMENDADO ALTERAR ELEMENTOS APENAS INTERAGIR
+  @ViewChild('teste', { static: false }) mensagemTela: ElementRef | undefined
+  //Extrair componente da sua view
+  @ViewChild(ProdutoCountComponent, { static: false }) contador: ProdutoCountComponent | undefined
+
+  @ViewChildren(ProdutoCardDetalheComponent) botoes: QueryList<ProdutoCardDetalheComponent> | undefined;
 
   constructor() { }
 
@@ -54,6 +64,25 @@ export class ProdutoDashboardComponent {
       valor: 600,
       imagem: 'headset.jpg'
     }];
+  }
+
+  ngAfterViewInit(): void {
+    //Alterando componente filho da sua view, Teste apenas
+    setTimeout(() => {
+      console.log('objeto do contaodor: ', this.contador?.produtos);
+      this.contador?.produtos.splice(5, this.contador?.produtos.length);
+      console.log('objeto do contaodor deletado:  ', this.contador?.produtos);
+      console.log(this.botoes?.forEach(p => console.log(p.produto)));
+    }, 1000);
+
+    let clickTexto: Observable<any> = fromEvent(this.mensagemTela?.nativeElement, 'click');
+    clickTexto.subscribe((event: any) => { //Type is PointerEvent
+      console.log(event)
+      return;
+    });
+
+    //ViewChildren lista de itens nao modificados
+    console.log(this.botoes?.forEach(p => console.log(p.produto)));
   }
 
   mudarStatus(event: Produto): void {
