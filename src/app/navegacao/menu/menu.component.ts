146ca8373d398e10a9ba737ba2220e26bd2d561b
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2023-01-07 15:28:34 -0300

    feat: link de navegacao dinamicos

--- src/app/navegacao/menu/menu.component.ts
+++ src/app/navegacao/menu/menu.component.ts
@@ -1,7 +1,42 @@
 import { Component } from '@angular/core';
+import { Nav } from './models/nav';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html'
 })
-export class MenuComponent { }
+export class MenuComponent {
+
+  nav: Nav[] = [
+    {
+      link: '/home',
+      name: 'Home',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/cadastro',
+      name: 'Cadastro',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/sobre',
+      name: 'Sobre',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/produtos',
+      name: 'Produtos',
+      exact: false,
+      admin: false
+    },
+    {
+      link: '/admin',
+      name: 'Admin',
+      exact: false,
+      admin: false
+    }
+  ];
+}
